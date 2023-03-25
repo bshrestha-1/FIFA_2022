4ec69bf53d3185e5a51f4c0fb668b07444d121c0
Bijay Shrestha <bshrestha-1@ou.edu>
2023-03-25 14:00:00 +0100

    ADDED libs

--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "Enter the day for the commit (e.g., 'Mon', 'Tue', 'Wed', etc.):"
-read day
-git add FIFA2022_analysis.sas
-git commit -m "fixed some issues" --date="$day 14:00 2023 +0100" --no-edit
-git push
-
-echo "Git operations completed successfully."
